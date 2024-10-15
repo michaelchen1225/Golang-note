d9ceb80e5720825798f710597a562ff06921595f
michaelchen1225 <90543072+michaelchen1225@users.noreply.github.com>
2024-10-15 18:46:52 +0800

    update: if

--- 01-test/test.go
+++ 01-test/test.go
@@ -1,23 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
-var x = "pkg" // package scope
+var x int
 
-func main() {
-	fmt.Println("Main Scope:", x) // read from package scope
+func inputAndCheck(input int) error {
 
-	x := "main" // redeclare x in main scope
-
-	fmt.Println("Main Scope:", x) // read from main scope
-
-	if true {
-		fmt.Println("If Scope:", x) // read from main scope
-		printX()
+	if input < 7 || input > 20 {
+		return errors.New("輸入錯誤，請輸入 7 ~ 20 之間的整數")
+	} else {
+		return nil
 	}
-
 }
 
-func printX() {
-	fmt.Println("Function Scope:", x) // read from package scope
+func main() {
+	fmt.Print("請輸入一個介於 7 ~ 20 的整數：")
+	_, error := fmt.Scanf("%d", &x)
+
+	if error != nil {
+		fmt.Println("輸入錯誤，請輸入整數")
+		log.Fatal(error)
+	} else if error := inputAndCheck(x); error != nil {
+		fmt.Println(error)
+	} else if x%2 == 0 {
+		fmt.Println("偶數")
+	} else {
+		fmt.Println("奇數")
+	}
 }
