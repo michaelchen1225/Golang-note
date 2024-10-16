59313ef08a85c52f55b58e02528e7510adb12b3c
michaelchen1225 <90543072+michaelchen1225@users.noreply.github.com>
2024-10-16 14:47:03 +0800

    update 03

--- 01-test/test.go
+++ 01-test/test.go
@@ -1,34 +1,11 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"log"
-)
-
-var x int
-
-func inputAndCheck(input int) error {
-
-	if input < 7 || input > 20 {
-		return errors.New("輸入錯誤，請輸入 7 ~ 20 之間的整數")
-	} else {
-		return nil
-	}
-}
+import "fmt"
 
 func main() {
-	fmt.Print("請輸入一個介於 7 ~ 20 的整數：")
-	_, error := fmt.Scanf("%d", &x)
 
-	if error != nil {
-		fmt.Println("輸入錯誤，請輸入整數")
-		log.Fatal(error)
-	} else if error := inputAndCheck(x); error != nil {
-		fmt.Println(error)
-	} else if x%2 == 0 {
-		fmt.Println("偶數")
-	} else {
-		fmt.Println("奇數")
+	for i, c := range "hello" {
+		fmt.Printf("%d: %c\n", i, c)
 	}
+
 }
